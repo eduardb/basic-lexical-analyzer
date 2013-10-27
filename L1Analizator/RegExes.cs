4202210f8bde3089120837dcdf09f6ed02293224
eduardb <edy@fellan-soft.com>
2013-10-27 15:17:37 +0200

    removed more unused stuff + added class diagram

--- L1Analizator/RegExes.cs
+++ L1Analizator/RegExes.cs
@@ -12,17 +12,8 @@ namespace L1Analizator
         public static string[] KeywordsArray = { "while", "if", "else", "std", "cin", "cout", "int", "float" };
         public static string[] OperatorsArray = { "+", "-", "*", "/" };
         public static string[] RelationsArray = { "<", "<=", "==", "!=", ">=", ">" };
-        private static Regex arrayLengthRegex;
         private static Regex constantRegex;
         private static Regex identifierRegex;
-        private static Regex numericalIndexRegex;
-
-        public static Regex getArrayLengthRegex()
-        {
-            if (arrayLengthRegex == null)
-                arrayLengthRegex = new Regex(@"^[1-9]\d*$");
-            return arrayLengthRegex;
-        }
 
         public static Regex getConstantRegex()
         {
@@ -38,21 +29,9 @@ namespace L1Analizator
             return identifierRegex;
         }
 
-        public static Regex getNumericalIndexRegex()
-        {
-            if (numericalIndexRegex == null)
-                numericalIndexRegex = new Regex(@"^(0|[1-9]\d*)$");
-            return numericalIndexRegex;
-        }
-
         public static bool isIdentifier(string atom)
         {
             return getIdentifierRegex().IsMatch(atom) && !KeywordsArray.Contains(atom);
-        }
-
-        public static bool isIndex(string atom)
-        {
-            return getNumericalIndexRegex().IsMatch(atom) || getIdentifierRegex().IsMatch(atom);
         }
 
         public static bool isRelation(string atom)
